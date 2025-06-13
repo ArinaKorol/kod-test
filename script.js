55d50de6d8bdf9906f66c35d64f10cf18a64e51d
ArinaKorol <k0roleva.arin@yandex.ru>
2025-06-13 08:24:52 +0300

    task-4

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let btn = document.getElementById('btn');
+let button = document.querySelector("button")
+let list = document.querySelector('ul')
 
-btn.addEventListener("click", function() {
-    let year = parseInt(document.getElementById('year').value);
-    let currentYear = new Date().getFullYear();
-    let result = document.getElementById('result');
-    
-    if (isNaN(year) || year > currentYear)  {
-        result.textContent = 'Ошибка! Перепроверьте данные'
-    } else {
-        result.textContent = `Ваш возраст: ${currentYear - year}`
-    }
- });
+button.addEventListener("click", function(){
+  if (document.querySelector('input').value !== ''){
+    let task = document.createElement("li");
+    task.textContent = document.querySelector('input').value;
+    list.appendChild(task);
+    task.addEventListener('click', function(){
+      task.remove();
+    })
+  }
+})
+
+
+
